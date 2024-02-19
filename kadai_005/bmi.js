c1babef9d55864162172abae120d0990bc9d8a31
muu8938 <155262075+muu8938@users.noreply.github.com>
2024-02-20 06:32:52 +0900

    bmi.jsを訂正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,7 +1,12 @@
-let weight = 68;
-let height = 1.7;
+// 変数の宣言（体重と身長）
+let weight;
+let height;
 
-// bmi
+// 値の代入
+weight = 68;
+height = 1.7;
+
+// bmiの計算式
 let bmi = weight / (height * height);
 
 // コンソールへの出力
